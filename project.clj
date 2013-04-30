9297537fae323125894e46e7c9a2d978ac35d550
J. David Lowe <j.david.lowe@gmail.com>
2013-04-30 01:45:13 -0700

    progress.

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject bleach "0.0.1"
+(defproject bleach "0.0.4"
   :description "Whitens unsightly code"
   :url "http://github.com/dlowe/bleach"
   :license {:name "Artistic License" :url "http://dev.perl.org/licenses/artistic.html"}
   :eval-in-leiningen true
+  :aliases {"jar" ["trampoline" "jar"]
+            "push" ["trampoline" "push"]}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main bleach.core)
--- src/bleach/core.clj
+++ src/bleach/core.clj
@@ -1,8 +1,25 @@
 (ns bleach.core (:gen-class))
 
+(defn bits
+  [x]
+  (loop [x (int x)
+         bs ()]
+    (if (zero? x)
+      bs
+      (recur (bit-shift-right x 1) (conj bs (bit-and x 0x01))))))
+
 (defn whiten
   [shirt]
-  (list* (map int (str "(do " shirt ")"))))
+  (apply str (mapcat #(str (apply str %) "\n") (map bits (str "(do " shirt ")")))))
+
+(defn unbits
+  [bs]
+  (loop [x 0
+         bs bs]
+    (if (empty? bs)
+      x
+      (let [x (bit-shift-left x 1)]
+        (recur (if (pos? (first bs)) (bit-set x 0) x) (rest bs))))))
 
 (defn brighten
   [shirt]
