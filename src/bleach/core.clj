ed9ff07b891edcd6eb70d57eb2f9ab3a34a4acf9
J. David Lowe <j.david.lowe@gmail.com>
2013-05-01 18:50:13 -0700

    whitening :)

--- src/bleach/core.clj
+++ src/bleach/core.clj
@@ -1,29 +1,35 @@
-(ns bleach.core (:gen-class))
+(ns bleach.core
+  (:use [clojure.string])
+  (:gen-class))
 
 (defn bits
-  [x]
-  (loop [x (int x)
-         bs ()]
-    (if (zero? x)
-      bs
-      (recur (bit-shift-right x 1) (conj bs (bit-and x 0x01))))))
+  [x & bs]
+  (if (zero? x)
+    bs
+    (recur (bit-shift-right x 1) (conj bs (bit-and x 0x01)))))
 
 (defn whiten
   [shirt]
-  (apply str (mapcat #(str (apply str %) "\n") (map bits (str "(do " shirt ")")))))
+  (->>
+    (str "(do " shirt ")")
+    (mapcat (comp #(str (apply str %) "\n") (partial map #(if (= % 0) \space \tab)) bits int))
+    (apply str)))
 
 (defn unbits
   [bs]
-  (loop [x 0
-         bs bs]
-    (if (empty? bs)
-      x
-      (let [x (bit-shift-left x 1)]
-        (recur (if (pos? (first bs)) (bit-set x 0) x) (rest bs))))))
+  (reduce (fn [x bit]
+            (let [x (bit-shift-left x 1)]
+              (if (pos? bit)
+                (bit-set x 0)
+                x))) 0 bs))
 
 (defn brighten
   [shirt]
-  (read-string (apply str (map char shirt))))
+  (->>
+    (split-lines shirt)
+    (map (comp char unbits (partial map #(if (= % \tab) 1 0))))
+    (apply str)
+    (read-string)))
 
 (defn -main [file]
   (spit file (str "(use 'bleach.core) #bleach/ed " (whiten (slurp file)))))
