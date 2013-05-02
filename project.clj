17d4a105fbe82c2de3d172a1e3956615039a6e39
J. David Lowe <j.david.lowe@gmail.com>
2013-05-01 20:59:59 -0700

    clojure 1.4 is ok

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject bleach "0.0.10"
+(defproject bleach "0.0.11"
   :description "Whitens unsightly code"
   :url "http://github.com/dlowe/bleach"
   :license {:name "Artistic License" :url "http://dev.perl.org/licenses/artistic.html"}
   :eval-in-leiningen true
   :aliases {"jar" ["trampoline" "jar"]
             "push" ["trampoline" "push"]}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :main bleach.core)
